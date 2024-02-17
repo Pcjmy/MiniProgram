21ccdb16ada381b5076c98765a204c1515d4350e
Pcjmy <1716045191@qq.com>
2024-02-17 16:31:37 +0800

    feat: data extraction

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,13 @@
 Page({
   data: {
-    showIcon: true
+    showIcon: true,
+    swiperList: [{
+      imgUrl: '../../resources/course1.jpg',
+    }, {
+      imgUrl: '../../resources/course2.png',
+    }, {
+      imgUrl: '../../resources/course3.png',
+    }],
   },
   handleInputChange(e) {
     const value = e.detail.value;
